e3f4bf497eb29dd742b48e6c842f491f0eaa1a6b
Miljan Milivojevic <Miljan.Milivojevic@msg-global.com>
2023-06-12 23:51:37 +0200

    Kreirana prva strana, heder i futer

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans|Candal|Alegreya+Sans">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/imagehover.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Kolaci</title>
+</head>
+<body>
+ 
+    <nav class="navbar navbar-expand-lg border navbar-dark sticky-top bg-dark">
+        <a class="navbar-brand" href="index.php">Komad kolač</a>
+        <div class="navbar-collapse collapse">
+            <ul class="navbar-nav mx-auto">
+               
+                <li class="nav-item mx-auto">
+                    <a class="nav-link" href="index.php">Početna</a>
+                </li>
+                <li class="nav-item mx-auto">
+                    <a class="nav-link" href="proizvodi.php">Porudžbina</a>
+                </li>
+                <li class="nav-item mx-auto">
+                    <a class="nav-link" href="kreirajProizvod.php">Kreiraj porudžbinu</a>
+                </li>
+                <li class="nav-item mx-auto">
+                    <a class="nav-link" href="kategorije.php">Kategorije</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
